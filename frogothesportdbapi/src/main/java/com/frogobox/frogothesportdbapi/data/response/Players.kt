77194b18fd9ec6cf236fb32f8b8dcf46f1472381
amirisback <faisalamircs@gmail.com>
2020-03-06 17:02:42 +0700

    add: Player Model

---
+++ frogothesportdbapi/src/main/java/com/frogobox/frogothesportdbapi/data/response/Players.kt
@@ -0,0 +1,20 @@
+package com.frogobox.frogothesportdbapi.data.response
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * TheSportDBApi
+ * Copyright (C) 06/03/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.frogothesportdbapi.data.response
+ *
+ */
+data class Players(val player: List<Players>)
